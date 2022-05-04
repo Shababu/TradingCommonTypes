000af8afe0df34d063919791d65b9cb407c361fd
Александр <skinnyrus@mail.ru>
2022-05-04 07:36:22 +0300

    1. Added PlaceInitialOrders() method. 2. Removed IncreaseAmount() method 3. Removed UpdateOrderStatusAndId() method

--- TradingCommonTypes/ITrader.cs
+++ TradingCommonTypes/ITrader.cs
@@ -1,18 +1,14 @@
-﻿using System.Collections.Generic;
-
-namespace TradingCommonTypes
+﻿namespace TradingCommonTypes
 {
     public interface ITrader
     {
         ILimitOrder PlaceNewLimitOrder(IExchangeUser user, string symbol, Sides side, decimal quantity, decimal price);
-        void AutoTrade(IExchangeUser user, List<SpotPosition> positions, bool increaseAmount);
-        void UpdateOrderStatusAndId(SpotPosition position, ILimitOrder newLimitOrder);
-        void IncreaseAmount(SpotPosition position);
+        ILimitOrder PlaceNewMarketOrder(IExchangeUser user, string symbol, Sides side, decimal quantity);
         ILimitOrder GetOrderInfo(IExchangeUser user, string orderId, string symbol);
         string GetOrderStatus(IExchangeUser user, SpotPosition position, string symbol);
-        ILimitOrder PlaceNewMarketOrder(IExchangeUser user, string symbol, Sides side, decimal quantity);
         List<ILimitOrder> GetOpenOrders(IExchangeUser user, string symbol);
-        ILimitOrder CancelLimitOrder(IExchangeUser user, string symbol, string orderId); 
-
+        void PlaceInitialOrders(IExchangeUser user, List<SpotPosition> positions);
+        void AutoTrade(IExchangeUser user, List<SpotPosition> positions, bool increaseAmount);
+        void UpdateOrderStatusAndId(SpotPosition position, ILimitOrder newLimitOrder);
     }
 }
