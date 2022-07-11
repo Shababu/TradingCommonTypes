141a5289ff6463959c6f3df059e6a2f0f2c53d15
Александр <skinnyrus@mail.ru>
2022-07-12 02:15:23 +0300

    Added IWithdrawal interface and GetRecentWithdrawals() method into IWalletInfo interface

--- TradingCommonTypes/IWalletInfo.cs
+++ TradingCommonTypes/IWalletInfo.cs
@@ -5,5 +5,6 @@
         List<ICryptoBalance> GetWalletInfo(IExchangeUser user);
         decimal GetAccountTotalBalance(List<ICryptoBalance> balances);
         List<IDeposit> GetRecentDeposits(IExchangeUser user, string coin, DateTime startTime, DateTime endTime);
+        List<IWithdrawal> GetRecentWithdrawals(IExchangeUser user, string coin, DateTime startTime, DateTime endTime);
     }
 }
---
+++ TradingCommonTypes/IWithdrawal.cs
@@ -0,0 +1,14 @@
+﻿namespace TradingCommonTypes
+{
+    public interface IWithdrawal
+    {
+        string Id { get; set; }
+        decimal Amount { get; set; }
+        string TransactionFee { get; set; }
+        string Coin { get; set; }
+        int Status { get; set; }
+        string Address { get; set; }
+        string TxId { get; set; }
+        DateTime ApplyTime { get; set; }
+    }
+}
